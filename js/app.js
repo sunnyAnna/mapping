76b4bccfc6616acf42e25df2410225cb90456efc
Anna <annakrygier@hotmail.com>
2016-07-28 11:06:26 -0700

    added css files and npm oauth plugin

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var model = function () {
+	var map;
+
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: 41.3099,
+				lng: 122.3106
+			},
+			zoom: 10
+		});
+	}
+}
+
+
+OAuth.initialize('UxD0Z5qqLISGMYhh-RhlLw');
