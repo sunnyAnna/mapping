1385f5daabbfe238174dd26d032855986bf1d8a3
Anna <annakrygier@hotmail.com>
2016-08-09 16:38:54 -0700

    configured basic display list with nearby meetups

--- js/app.js
+++ js/app.js
@@ -1,79 +1,93 @@
 var map, marker, infowindow, circle, geocoder, jso;
 
-var model = {
-	init: function () {},
-	mapData: {
-		newMarker: function () {
-			marker = new google.maps.Marker({
-				position: map.center,
-				map: map,
-				//animation: DROP,
-				title: 'you are here'
-			});
-		},
-		newInfoWindow: function () {
-			infowindow = new google.maps.InfoWindow({
-				content: ''
-			});
-		},
-		newCircle: function () {
-			circle = new google.maps.Circle({
-				center: map.center,
-				map: map,
-				radius: ViewModel.radius() * 1609.34,
-				fillColor: '#fff',
-				fillOpacity: 0.5
-			});
-		},
-		stylesArray: [
-			{
-				"elementType": "labels",
-				"stylers": [
-					{
-						"visibility": "off"
+var MapModel = {
+	init: function () {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: 37.7749,
+				lng: -122.4194
+			},
+			zoom: 12
+		});
+		map.setOptions({
+			styles: MapModel.stylesArray
+		});
+		geocoder = new google.maps.Geocoder();
+		MapModel.newMarker();
+		MapModel.newInfoWindow();
+		MapModel.newCircle();
+		ViewModel.addEventListeners();
+	},
+	newMarker: function () {
+		marker = new google.maps.Marker({
+			position: map.center,
+			map: map,
+			//animation: DROP,
+			title: 'you are here'
+		});
+	},
+	newInfoWindow: function () {
+		infowindow = new google.maps.InfoWindow({
+			content: ''
+		});
+	},
+	newCircle: function () {
+		circle = new google.maps.Circle({
+			center: map.center,
+			map: map,
+			radius: ViewModel.radius() * 1609.34,
+			fillColor: '#fff',
+			fillOpacity: 0.5
+		});
+	},
+	stylesArray: [
+		{
+			"elementType": "labels",
+			"stylers": [
+				{
+					"visibility": "off"
 						}
 					]
 				},
-			{
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"visibility": "off"
+		{
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "off"
 						}
 					]
 				},
-			{
-				"featureType": "road",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"visibility": "on"
+		{
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "on"
 						},
-					{
-						"color": "#000000"
+				{
+					"color": "#000000"
 						}
 					]
 				},
-			{
-				"featureType": "landscape",
-				"stylers": [
-					{
-						"color": "#ffffff"
+		{
+			"featureType": "landscape",
+			"stylers": [
+				{
+					"color": "#ffffff"
 						},
-					{
-						"visibility": "on"
+				{
+					"visibility": "on"
 						}
 					]
 				}
 			]
-	}
 }
 
 
 var ViewModel = {
 	radius: ko.observable(2),
 	address: ko.observable(''),
-	zip: 94939,
+	zip: '',
 	info: ko.observable(''),
 	item: function (x) {
 		this.addr = ko.observable(x);
@@ -81,47 +95,49 @@ var ViewModel = {
 	addressList: ko.observableArray([]),
 	meetupList: ko.observableArray([]),
 	meetupGroup: function (data) {
-		this.eventName = title;
-		this.eventUrl = url;
-		this.groupName = name;
-		this.groupPhoto = photo;
-		this.venue = venue;
-		this.yesRsvpCount = rsvp;
+		this.eventName = ko.observable(data.name);
+		this.url = ko.observable(data.event_url);
+		this.groupName = ko.observable(data.group.name);
+		this.venue = {
+			street: data.address_1,
+			city: data.city,
+			state: data.state,
+			country: data.country,
+			zip: data.zip
+		};
+		this.imgSrc = '';
 	},
-	init: function () {
-		model.init();
-		MapView.init();
-		//meetup.init();
-		ViewModel.addEventListeners();
+	showMoreInfo: function (x) {
+		ViewModel.meetupInfo(x)
 	},
-	createMarker: function () {
-		return model.mapData.newMarker();
-	},
-	createCircle: function () {
-		return model.mapData.newCircle();
-	},
-	createInfoWindow: function () {
-		return model.mapData.newInfoWindow();
-	},
+	meetupInfo: ko.observable(),
 	findAddress: function () {
 		return geocoderView.geocodeAddress();
 	},
 	findZipCode: function (arr) {
 		arr.forEach(function (x) {
 			if (x.types[0] === "postal_code") {
-				return ViewModel.zip = x.short_name;
+				ViewModel.zip = x.short_name;
+				ViewModel.updatePosition('marker');
+				ViewModel.updatePosition('circle');
+				ViewModel.updateCircle();
+				ViewModel.getAttractions();
+				return;
 			}
 		});
+
 	},
 	addEventListeners: function () {
 		marker.onclick = function (e) {
 			e.preventDefault();
 			infowindow.open(map, marker);
-			meetup.init();
 		}
 	},
-	makeList: function (x) {
+	makeAddrList: function (x) {
 		ViewModel.addressList.push(new ViewModel.item(x.formatted_address));
+	},
+	makeMtpList: function (x) {
+		ViewModel.meetupList.push(new ViewModel.meetupGroup(x));
 	},
 	setAddress: function (y) {
 		ViewModel.address(y.addr());
@@ -142,6 +158,7 @@ var ViewModel = {
 			circle.setOptions({
 				center: map.center
 			});
+			break;
 		}
 	},
 	updateCircle: function () {
@@ -150,18 +167,27 @@ var ViewModel = {
 		});
 	},
 	getAttractions: function () {
-		return meetup.init();
+		return meetup.APIcall();
 	}
 }
 
 
 var meetup = {
+	init: function () {
+		JSO.enablejQuery($);
+		meetup.newJso();
+		if (!window.location.hash) {
+			meetup.tokenRequest();
+		}
+		meetup.tokenSave();
+		//meetup.APIcall();
+	},
 	newJso: function () {
 		jso = new JSO({
 			providerID: "meetup",
 			client_id: "at0i8rfnm3p5nqphdjg9acn0hu",
 			authorization: "https://secure.meetup.com/oauth2/authorize",
-			redirect_uri: "http://127.0.0.1:59198/neighborhood-map/index.html",
+			redirect_uri: "http://127.0.0.1:60535/neighborhood-map/index.html",
 			response_type: "token"
 		});
 	},
@@ -170,10 +196,9 @@ var meetup = {
 		jso.ajax({
 			url: url,
 			dataType: 'jsonp',
-			jsonCallback: meetup.cb,
 			success: function (data) {
 				console.log("Success response (meetup):");
-				console.log(data);
+				meetup.cb(data);
 			},
 			failure: function (err) {
 				console.log("Error response (meetup):");
@@ -183,50 +208,22 @@ var meetup = {
 	},
 	tokenRequest: function () {
 		jso.getToken(function (token) {
-			meetup.APIcall();
+			//meetup.APIcall();
 		});
 	},
 	tokenSave: function () {
 		jso.callback(window.location.href, meetup.APIcall, jso.providerID);
 	},
-	init: function () {
-		JSO.enablejQuery($);
-		meetup.newJso();
-		if (!window.location.hash) {
-			meetup.tokenRequest();
-		}
-		meetup.tokenSave();
-		meetup.APIcall();
-	},
-	cb: function () {
-		console.log('done');
+	cb: function (data) {
+		console.log(data);
+		var results = data.results;
+		results.forEach(ViewModel.makeMtpList);
 	},
 	getData: function () {
 
 	}
 }
 
-
-
-var MapView = {
-	init: function () {
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {
-				lat: 37.7749,
-				lng: -122.4194
-			},
-			zoom: 12
-		});
-		map.setOptions({
-			styles: model.mapData.stylesArray
-		});
-		geocoder = new google.maps.Geocoder();
-		ViewModel.createMarker();
-		ViewModel.createInfoWindow();
-		ViewModel.createCircle();
-		//ViewModel.addEventListeners();
-	}
-}
 
 
 var geocoderView = {
@@ -241,14 +238,10 @@ var geocoderView = {
 			}, function (results, status) {
 				if (status == google.maps.GeocoderStatus.OK && results.length > 1) {
 					ViewModel.info('Did you mean:');
-					results.forEach(ViewModel.makeList);
+					results.forEach(ViewModel.makeAddrList);
 				} else if (status == google.maps.GeocoderStatus.OK) {
 					map.setCenter(results[0].geometry.location);
 					ViewModel.findZipCode(results[0].address_components);
-					ViewModel.updatePosition('marker');
-					ViewModel.updatePosition('circle');
-					ViewModel.updateCircle();
-					ViewModel.getAttractions();
 				} else {
 					ViewModel.info('We could not find that location - try entering a more specific place.');
 				}
@@ -261,5 +254,6 @@ ko.applyBindings(ViewModel);
 
 function generator() {
 	//localStorage.clear();
-	ViewModel.init();
+	meetup.init();
+	MapModel.init();
 };
